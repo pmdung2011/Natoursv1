90b889e3dcaec566f26131a5b300ef009eb28da4
Danny Phung <pmdung2011@gmail.com>
2021-01-24 16:19:27 -0600

    'Finalized'

---
+++ public/js/submitReview.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+export const submitReview = async (review, rating) => {
+  try {
+    const slug = window.location.href.split('/')[4];
+
+    const res = await axios({
+      method: 'POST',
+      url: '/api/v1/reviews',
+      data: {
+        review,
+        rating,
+        slug
+      }
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'Review successfully submitted!');
+
+      setTimeout(() => {
+        window.location.reload();
+        window.scroll({
+          top: document.body.scrollHeight * 0.54,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert('error', err.message);
+  }
+};
